759ede21e2497a5962ada21faa577225b7738fd1
Rishi Jha <rishijha2312@gmail.com>
2026-02-06 18:48:05 +0530

    Array chunking

---
+++ arrayChunking.js
@@ -0,0 +1,15 @@
+function chunkArray(arr, n) {
+    // Your implementation
+      if (n <= 0) return [];
+    
+    const result = [];
+    for (let i = 0; i < arr.length; i += n) {
+        result.push(arr.slice(i, i + n));
+    }
+    return result;
+}
+
+//For the purpose of user debugging.
+//pass your array and chunk size in function call
+chunkArray([1, 2, 3], 5);
+module.exports = chunkArray
